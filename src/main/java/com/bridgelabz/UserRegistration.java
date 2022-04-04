6220de232c182f67d43d676d4bb25af5aa466dfc
Shubham <ssp671995@gmail.com>
2022-04-04 22:52:05 +0530

    Added the welcome message in master.

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+/**
+ * User Registration System needs to ensure all validations are in place during the User Entry.
+ * @author Shubham Pawar
+ * @since 03-04-2021
+ */
+public class UserRegistration {
+}
+
---
+++ src/test/java/com/bridgelababz/UserRegistrationTest.java
@@ -0,0 +1,8 @@
+package com.bridgelababz;
+import com.bridgelabz.UserRegistration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+
+}
